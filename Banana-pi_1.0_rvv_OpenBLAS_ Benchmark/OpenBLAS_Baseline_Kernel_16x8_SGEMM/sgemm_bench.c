7fbafc9a2c6d569fa3c2e2870200f21bce93c235
Babar Hussain <b.hussain@studenti.unisa.it>
2026-02-26 01:49:52 -0800

    Update sgemm_bench.c

--- Banana-pi_1.0_rvv_OpenBLAS_ Benchmark/OpenBLAS_Baseline_Kernel_16x8_SGEMM/sgemm_bench.c
+++ Banana-pi_1.0_rvv_OpenBLAS_ Benchmark/OpenBLAS_Baseline_Kernel_16x8_SGEMM/sgemm_bench.c
@@ -7,8 +7,8 @@
 typedef long BLASLONG;
 typedef float FLOAT;
 
-/* Kernel declaration: LMUL=1 + Unroll×4 PRAGMA */
-int sgemm_kernel_16x8_zvl256b_lmul1_opt_unroll4(
+/* Kernel declaration */
+int sgemm_kernel_16x8_zvl256b_lmul1_opt(
     BLASLONG M, BLASLONG N, BLASLONG K,
     FLOAT alpha, FLOAT *A, FLOAT *B, FLOAT *C, BLASLONG ldc
 );
@@ -22,67 +22,48 @@ static double now_sec(void)
 
 static void fill(float *x, size_t n)
 {
-    for (size_t i = 0; i < n; i++)
-        x[i] = (float)((int)(i % 13) - 6) * 0.1f;
+    for(size_t i=0;i<n;i++)
+        x[i]=(float)((i%13)-6)*0.1f;
 }
 
-int main(int argc, char **argv)
+int main(int argc,char**argv)
 {
-    if (argc < 4) {
-        printf("Usage: %s M N K\n", argv[0]);
+    if(argc<4){
+        printf("Usage: %s M N K\n",argv[0]);
         return 0;
     }
 
-    BLASLONG M = atol(argv[1]);
-    BLASLONG N = atol(argv[2]);
-    BLASLONG K = atol(argv[3]);
+    BLASLONG M=atol(argv[1]);
+    BLASLONG N=atol(argv[2]);
+    BLASLONG K=atol(argv[3]);
 
-    size_t sizeA = (size_t)M * (size_t)K;
-    size_t sizeB = (size_t)N * (size_t)K;
-    size_t sizeC = (size_t)M * (size_t)N;
+    size_t sizeA=(size_t)M*K;
+    size_t sizeB=(size_t)N*K;
+    size_t sizeC=(size_t)M*N;
 
-    size_t bytesA = sizeA * sizeof(FLOAT);
-    size_t bytesB = sizeB * sizeof(FLOAT);
-    size_t bytesC = sizeC * sizeof(FLOAT);
+    FLOAT*A=aligned_alloc(64,sizeA*sizeof(FLOAT));
+    FLOAT*B=aligned_alloc(64,sizeB*sizeof(FLOAT));
+    FLOAT*C=aligned_alloc(64,sizeC*sizeof(FLOAT));
 
-    size_t padA = (bytesA + 63u) & ~((size_t)63u);
-    size_t padB = (bytesB + 63u) & ~((size_t)63u);
-    size_t padC = (bytesC + 63u) & ~((size_t)63u);
+    fill(A,sizeA);
+    fill(B,sizeB);
+    fill(C,sizeC);
 
-    FLOAT *A = (FLOAT*)aligned_alloc(64, padA);
-    FLOAT *B = (FLOAT*)aligned_alloc(64, padB);
-    FLOAT *C = (FLOAT*)aligned_alloc(64, padC);
+    double t0=now_sec();
 
-    if (!A || !B || !C) {
-        fprintf(stderr, "Allocation failed\n");
-        return 1;
-    }
+    sgemm_kernel_16x8_zvl256b_lmul1_opt(
+        M,N,K,1.0f,A,B,C,M);
 
-    fill(A, sizeA);
-    fill(B, sizeB);
-    fill(C, sizeC);
+    double t1=now_sec();
 
-    double t0 = now_sec();
+    double time=t1-t0;
 
-    sgemm_kernel_16x8_zvl256b_lmul1_opt_unroll4(
-        M, N, K, 1.0f, A, B, C, M
-    );
+    double gflops=
+        (2.0*(double)M*N*K)/(time*1e9);
 
-    double t1 = now_sec();
+    printf("M=%ld N=%ld K=%ld\n",M,N,K);
+    printf("Time: %.6f sec\n",time);
+    printf("GFLOPS: %.4f\n",gflops);
 
-    double time = t1 - t0;
-
-    double gflops =
-        (2.0 * (double)M * (double)N * (double)K)
-        / (time * 1e9);
-
-    printf("M=%ld N=%ld K=%ld\n", M, N, K);
-    printf("Time: %.6f sec\n", time);
-    printf("GFLOPS: %.4f\n", gflops);
-
-    free(A);
-    free(B);
-    free(C);
-
-    return 0;
+    free(A); free(B); free(C);
 }
